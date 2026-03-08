a7d1fc366f2403abfae26f239b45cd309ab963f7
Pratham1603 <prathamharer1603@gmail.com>
2026-03-08 20:47:49 +0530

    documenting

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mokshfit-chatbot",
+    version="0.1.0",
+    author="Pratham Gopal Harer",
+    author_email="prathamharer1603@gmail.com", 
+    packages=find_packages(),
+    install_requires=[]  
+)
